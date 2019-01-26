f75c57f98328d0f2bc3c28abeaf99e3d8b917fd3
juhyun10 <juhyun1010@naver.com>
2019-01-26 18:50:25 +0900

    unit12~15 dict, if

---
+++ unit13/13judge.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = input()
+
+if b == 'Cash3000':
+    c = a-3000
+elif b == 'Cash5000':
+    c = a-5000
+
+print(c)
+
---
+++ unit14/14prac.py
@@ -0,0 +1,7 @@
+writeten_test = 75
+coding_test = True
+
+if writeten_test >= 80 and coding_test:
+    print('pass')
+else:
+    print('fail')
